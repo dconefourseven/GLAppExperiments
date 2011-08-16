83ca36106e63ba16e1677ff93b86d1691bb92868
davidc14 <davidclarke1990@gmail.com>
2011-08-16 14:53:36 +0100

    Created big char map of hate. Will eventually export to file

--- GLApp/Sprites/SpriteFont/SpriteFont.m
+++ GLApp/Sprites/SpriteFont/SpriteFont.m
@@ -71,19 +71,83 @@ static const char charMap[] = {
     };
     
     NSDictionary *dictionary = [[NSDictionary alloc] initWithObjectsAndKeys:
-                                @"one", [NSNumber numberWithInt: 1],
-                                @"two", [NSNumber numberWithInt: 2],
-                                @"three", [NSNumber numberWithInt: 3],
+                                @"0", @"A",
+                                @"1", @"B",
+                                @"2", @"C",
+                                @"3", @"D",
+                                @"4", @"E",
+                                @"5", @"F",
+                                @"6", @"G",
+                                @"7", @"H",
+                                @"8", @"I",
+                                @"9", @"J",
+                                @"10", @"K",
+                                @"11", @"L",
+                                @"12", @"M",
+                                @"13", @"N",
+                                @"14", @"O",
+                                @"15", @"P",
+                                @"16", @"Q",
+                                @"17", @"R",
+                                @"18", @"S",
+                                @"19", @"T",
+                                @"20", @"U",
+                                @"21", @"V",
+                                @"22", @"W",
+                                @"23", @"X",
+                                @"24", @"Y",
+                                @"25", @"Z",
+                                @"26", @"a",
+                                @"27", @"b",
+                                @"28", @"c",
+                                @"29", @"d",
+                                @"30", @"e",
+                                @"31", @"f",
+                                @"32", @"g",
+                                @"33", @"h",
+                                @"34", @"i",
+                                @"35", @"j",
+                                @"36", @"k",
+                                @"37", @"l",
+                                @"38", @"m",
+                                @"39", @"n",
+                                @"40", @"o",
+                                @"41", @"p",
+                                @"42", @"q",
+                                @"43", @"r",
+                                @"44", @"s",
+                                @"45", @"t",
+                                @"46", @"u",
+                                @"47", @"v",
+                                @"48", @"w",
+                                @"49", @"x",
+                                @"50", @"y",
+                                @"51", @"z",
+                                @"52", @"0",
+                                @"53", @"1",
+                                @"54", @"2",
+                                @"55", @"3",
+                                @"56", @"4",
+                                @"57", @"5",
+                                @"58", @"6",
+                                @"59", @"7",
+                                @"60", @"8",
+                                @"61", @"9",
+                                @"62", @"+",
+                                @"63", @"-",
                                 nil];
     
     mSprites = [[NSMutableArray alloc]initWithCapacity:stringSize];
     
-    //NSNumber* test = [[NSNumber alloc]initWithInteger:[dictionary valueForKey:@"one"]];  
-    
-    //printf("%d", test.intValue);
-    
     for(int i = 0; i < stringSize; i++)
     {
+        //int positionInCharMap = 0;
+        char characterAtIndex = [stringText characterAtIndex:(NSUInteger)i];
+        NSString* charInString = [[NSString alloc]initWithCString:&characterAtIndex];
+         
+        NSString *test = [[NSString alloc]initWithString:[dictionary objectForKey:charInString]]; 
+        
+        
         
         Sprite* sprite = [[Sprite alloc]init:@"SpriteSheet.jpg":(GLfloat*)s_spriteTexcoords];
         
@@ -91,6 +155,9 @@ static const char charMap[] = {
         
         [sprite release];
     }
+    
+    [dictionary release];
+    [mSprites release];
 }
 
 -(void)DrawFont
